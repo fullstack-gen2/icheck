98b9aab23daabe3c43a8f2a3c304d389a0d0ed6a
thornie04 <kimchanthon46@gmail.com>
2026-06-15 23:09:58 +0700

    update form's width.

--- src/components/classroom-form-dialog.tsx
+++ src/components/classroom-form-dialog.tsx
@@ -253,7 +253,7 @@ export function ClassroomFormDialog({ open, initial, onOpenChange, onSaved }: Pr
 
   return (
     <Dialog open={open} onOpenChange={handleOpenChange}>
-      <DialogContent className="w-[90vw] max-w-5xl">
+      <DialogContent className="w-[75vw] sm:max-w-2xl ">
         <DialogHeader>
           <DialogTitle className="text-xl font-bold">{editing ? "Edit Class" : "Create Class"}</DialogTitle>
           <DialogDescription>
